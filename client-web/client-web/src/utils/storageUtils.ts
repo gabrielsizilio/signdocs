5295f0dc62a89ff8978bdf4ffe4635f2215373f7
Gabriel Sizilio <gabrielsizilio15@gmail.com>
2024-12-04 10:00:01 -0300

    feat: add storageUtils

---
+++ client-web/client-web/src/utils/storageUtils.ts
@@ -0,0 +1,14 @@
+export function setUserLocalStorage(user: IUser | null) {
+    localStorage.setItem("tk", JSON.stringify(user));
+}
+
+export function getUserLocalStorage() {
+    const json = localStorage.getItem("tk");
+
+    if (!json) {
+        return null;
+    }
+
+    const user = JSON.parse(json);
+    return user ?? null;
+}
